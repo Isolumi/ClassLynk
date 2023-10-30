96979b3ffa4ee7eed60a667a350d70e9dee21c92
Shi Hai <shihai797@gmail.com>
2023-10-30 17:44:34 -0400

    renamed file to align with the naming for interfaces

--- src/main/java/use_case/GenerateStaticImage/GenerateStaticImageDataAccessInterface.java
+++ src/main/java/use_case/GenerateStaticImage/GenerateStaticImageDataAccessInterface.java
@@ -3,7 +3,7 @@ package use_case.GenerateStaticImage;
 import java.util.ArrayList;
 import entity.Timetable;
 
-public interface GenerateStaticImage {
+public interface GenerateStaticImageDataAccessInterface {
 
     //read documentation for static maps image api and put method in
     ArrayList<String> getStaticMaps(Timetable Timetable);
