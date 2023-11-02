5b6887d741e8d8ba0c5705233ded9a77f96c97ad
Isolumi <hubertx98@gmail.com>
2023-11-02 19:53:06 -0400

    modified timetable's attributes to be a map of strings to a list of classes

--- src/main/java/entity/Timetable.java
+++ src/main/java/entity/Timetable.java
@@ -1,19 +1,20 @@
 package entity;
 
 import java.util.List;
+import java.util.Map;
 
 public class Timetable {
-    private List<Class> classes;
+    private Map<String, List<Class>> classes;
 
-    public List<Class> getClasses() {
+    public Map<String, List<Class>> getClasses() {
         return classes;
     }
 
-    public void setClasses(List<Class> classes) {
+    public void setClasses(Map<String, List<Class>> classes) {
         this.classes = classes;
     }
 
-    public Timetable(List<Class> classes) {
+    public Timetable(Map<String, List<Class>> classes) {
         this.classes = classes;
     }
 }
