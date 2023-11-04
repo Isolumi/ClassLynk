0d8464bc4818a0bb3ff907c5233c8e89f3c48b8c
Isolumi <hubertx98@gmail.com>
2023-11-04 13:48:31 -0400

    modified parameters for generateTimetable and deleted .gitkeep

--- src/main/java/entity/OptimalTimetableCalculator.java
+++ src/main/java/entity/OptimalTimetableCalculator.java
@@ -1,10 +1,11 @@
 package entity;
 
+import data_access.APIDataAccessObject;
 import java.util.List;
 
 public class OptimalTimetableCalculator {
-    public static Timetable generateTimetable(List<Class> classes) {
-        // TODO implement
+    public static Timetable generateTimetable(List<Course> courses) {
+
         return null;
     }
 }
