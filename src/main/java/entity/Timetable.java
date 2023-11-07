4428ed30c2e393321c4aac2857dc1a6190c5e0be
Isolumi <hubertx98@gmail.com>
2023-11-07 01:40:51 -0500

    modified timetable class to work with spring data. need to workout mapping data still

--- src/main/java/entity/Timetable.java
+++ src/main/java/entity/Timetable.java
@@ -1,19 +1,19 @@
 package entity;
+
 import java.util.List;
 import java.util.Map;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 
+@Entity
 public class Timetable {
+    @Id
     private Map<String, List<Class>> classes;
 
     public Map<String, List<Class>> getClasses() {
         return classes;
     }
-
     public void setClasses(Map<String, List<Class>> classes) {
-        this.classes = classes;
-    }
-
-    public Timetable(Map<String, List<Class>> classes) {
         this.classes = classes;
     }
 }
