b802181c7815066e503935e8bc19f1ba86221377
Isolumi <hubertx98@gmail.com>
2023-11-07 16:57:20 -0500

    added configuration files for firebase

---
+++ src/main/java/app/AppConfig.java
@@ -0,0 +1,14 @@
+package app;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public Dotenv dotenv() {
+        return Dotenv.configure().load();
+    }
+}
---
+++ src/main/java/app/FirebaseConfig.java
@@ -0,0 +1,29 @@
+package app;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+@Configuration
+public class FirebaseConfig {
+    private final Dotenv dotenv;
+    public FirebaseConfig(Dotenv dotenv) {
+        this.dotenv = dotenv;
+    }
+    @Bean
+    public FirebaseApp firebaseApp() throws IOException {
+        String serviceAccountJsonPath = dotenv.get("SERVICE_ACCOUNT_JSON_PATH");
+        assert serviceAccountJsonPath != null;
+        FileInputStream serviceAccount = new FileInputStream(serviceAccountJsonPath);
+        FirebaseOptions options = new FirebaseOptions.Builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .build();
+        return FirebaseApp.initializeApp(options);
+    }
+}
