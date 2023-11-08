dd31e38bbad962f0ae26e2a9d8cfa3fe940ee324
Isolumi <hubertx98@gmail.com>
2023-11-07 23:17:04 -0500

    updated application.yml

--- src/main/java/app/ClassLynkApplication.java
+++ src/main/java/app/ClassLynkApplication.java
@@ -1,11 +1,29 @@
 package app;
 
+import com.google.api.core.ApiFuture;
+import com.google.api.core.ApiFutures;
+import com.google.cloud.firestore.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
 
 @SpringBootApplication
 public class ClassLynkApplication {
+    private final static Logger logger = LoggerFactory.getLogger(ClassLynkApplication.class);
     public static void main(String[] args) {
         SpringApplication.run(ClassLynkApplication.class, args);
+        WebClient.create("http://localhost:8080");
     }
 }
