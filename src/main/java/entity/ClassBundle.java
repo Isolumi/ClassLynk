8ae3ae7c12a210bf8dcfb347f3b61f2b1579c44b
Isolumi <hubertx98@gmail.com>
2023-11-08 21:05:23 -0500

    added getters and setters and constructor to ClassBundle entity

--- src/main/java/entity/ClassBundle.java
+++ src/main/java/entity/ClassBundle.java
@@ -6,4 +6,24 @@ public class ClassBundle {
     String id;
     List<SClass> classes;
 
+    public ClassBundle(String id, List<SClass> classes) {
+        this.id = id;
+        this.classes = classes;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public List<SClass> getClasses() {
+        return classes;
+    }
+
+    public void setClasses(List<SClass> classes) {
+        this.classes = classes;
+    }
 }
