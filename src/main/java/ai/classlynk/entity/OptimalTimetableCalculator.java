c85d1c60345379ba929d97a8859b603adefd9e56
Legion7\Ryan <ryzhang04@gmail.com>
2023-11-11 15:36:48 -0500

    finished time conflict detection, some work done on algorithm

--- src/main/java/ai/classlynk/entity/OptimalTimetableCalculator.java
+++ src/main/java/ai/classlynk/entity/OptimalTimetableCalculator.java
@@ -1,10 +1,53 @@
 package ai.classlynk.entity;
 
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class OptimalTimetableCalculator {
     public static Timetable generateTimetable(List<Course> courses) {
-
+        HashMap<String, ArrayList<Course[]> > validLectureTutorialCombos = new HashMap<>();
+        for(Course course: courses){
+            for(ClassBundle lec: course.getClassBundles()){
+                if(!lec.getClasses().get(0).isTutorial()){
+                    for(ClassBundle tut: course.getClassBundles()){
+                        if(tut.getClasses().get(0).isTutorial()){
+                            HashMap<String, ArrayList<SClass>> test = new HashMap<>();
+                            test.put("Monday", new ArrayList<SClass>());
+                            test.put("Tuesday", new ArrayList<SClass>());
+                            test.put("Wednesday", new ArrayList<SClass>());
+                            test.put("Thursday", new ArrayList<SClass>());
+                            test.put("Friday", new ArrayList<SClass>());
+                            test.put("Saturday", new ArrayList<SClass>());
+                            test.put("Sunday", new ArrayList<SClass>());
+                        }
+                    }
+                }
+            }
+        }
         return null;
+
+    }
+
+    private static boolean hasTimeConflict(Timetable timetable){
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+        for(String day: timetable.getClasses().keySet()){
+            for(SClass sClass1: timetable.getClasses().get(day)){
+                LocalTime startTime1 = LocalTime.parse(sClass1.getStartTime(), formatter);
+                LocalTime endTime1 = LocalTime.parse(sClass1.getEndTime(), formatter);
+                for(SClass sClass2: timetable.getClasses().get(day)){
+                    if (!sClass1.equals(sClass2)){
+                        LocalTime startTime2 = LocalTime.parse(sClass2.getStartTime(), formatter);
+                        LocalTime endTime2 = LocalTime.parse(sClass2.getEndTime(), formatter);
+                        if(!(endTime1.isBefore(startTime2) || endTime2.isBefore(startTime1))){
+                            return true;
+                        }
+                    }
+                }
+            }
+        }
+        return false;
     }
 }
