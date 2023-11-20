0b07d5700b2a4f8f1cbcb99c90c97b494600430d
Isolumi <hubertx98@gmail.com>
2023-11-20 15:36:51 -0500

    updaded classBundle attributes

--- src/main/java/ai/classlynk/entity/ClassBundle.java
+++ src/main/java/ai/classlynk/entity/ClassBundle.java
@@ -3,20 +3,21 @@ package ai.classlynk.entity;
 import java.util.List;
 
 public class ClassBundle {
-    String courseId;
+    // id for the specific lecture section (ex. lec0101)
+    String lectureId;
     List<SClass> classes;
 
     public ClassBundle(String courseId, List<SClass> classes) {
-        this.courseId = courseId;
+        this.lectureId = courseId;
         this.classes = classes;
     }
 
     public String getCourseId() {
-        return courseId;
+        return lectureId;
     }
 
     public void setCourseId(String courseId) {
-        this.courseId = courseId;
+        this.lectureId = courseId;
     }
 
     public List<SClass> getClasses() {
