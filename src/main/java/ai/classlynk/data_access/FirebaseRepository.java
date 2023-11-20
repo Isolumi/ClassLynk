26c765a61f8ab5993b3bf4c1e29e8adc5d6fbba6
Isolumi <hubertx98@gmail.com>
2023-11-20 13:49:58 -0500

    added implementation from use cases

--- src/main/java/ai/classlynk/data_access/FirebaseRepository.java
+++ src/main/java/ai/classlynk/data_access/FirebaseRepository.java
@@ -1,18 +1,22 @@
 package ai.classlynk.data_access;
 
+import ai.classlynk.entity.*;
+import ai.classlynk.use_case.GenerateStaticImage.GenerateStaticImageDataAccessInterface;
+import ai.classlynk.use_case.explore_courses.ExploreCoursesDataAccessInterface;
+import ai.classlynk.use_case.generate_timetable.TimetableGeneratorDataAccessInterface;
+import ai.classlynk.use_case.save_view_time_tables.SaveViewTimetablesDataAccessInterface;
 import com.google.api.core.ApiFuture;
 import com.google.cloud.firestore.Firestore;
 import com.google.cloud.firestore.WriteResult;
-import ai.classlynk.entity.ClassBundle;
-import ai.classlynk.entity.Course;
-import ai.classlynk.entity.SClass;
 import org.springframework.stereotype.Repository;
+
 import javax.annotation.Resource;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 @Repository
-public class FirebaseRepository {
+public class FirebaseRepository implements ExploreCoursesDataAccessInterface, TimetableGeneratorDataAccessInterface, GenerateStaticImageDataAccessInterface, SaveViewTimetablesDataAccessInterface {
     @Resource
     private Firestore firestore;
 
@@ -37,10 +41,45 @@ public class FirebaseRepository {
                 .set(course);
     }
 
+    @Override
+    public Map<String, String> getStaticMaps(Timetable Timetable) {
+        return null;
+    }
+
+    @Override
+    public Map<String, Course> loadCourses() {
+        return null;
+    }
+
+    @Override
+    public float getRouteLength(String origin, String destination) {
+        return 0;
+    }
+
+    @Override
+    public void save(User user, Timetable timetable) {
+
+    }
+
+    @Override
+    public ArrayList<Timetable> load(User user) {
+        return null;
+    }
+
+    @Override
+    public void delete(User user, Timetable timetable) {
+
+    }
+
+
+
     public void insertCourses() {
+        // ant100
         SClass lec2501 = new SClass("ant100", "lec2501", "18:00:00",
-        "20:00:00", "monday", "convocation hall",
-                "31 King's College Circle" + "Toronto, ON" +
-                "M5S 1A1", false);
+                "20:00:00", "thursday", "convocation hall",
+                "31 King's College Circle " + "Toronto, ON " + "M5S 1A1", false);
+        SClass lec5101 = new SClass("ant100", "lec5101", "18:00:00",
+                "20:00:00", "thursday", "convocation hall",
+                "31 King's College Circle " + "Toronto, ON " + "M5S 1A1", false);
     }
 }
