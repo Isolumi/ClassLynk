d5ca54d2835c2ed1b5c98f063515b198d63fff1e
Isolumi <hubertx98@gmail.com>
2023-11-20 16:01:12 -0500

    modified parameters of saveviewtimetable use case interface

--- src/main/java/ai/classlynk/use_case/save_view_time_tables/SaveViewTimetablesDataAccessInterface.java
+++ src/main/java/ai/classlynk/use_case/save_view_time_tables/SaveViewTimetablesDataAccessInterface.java
@@ -1,16 +1,16 @@
 package ai.classlynk.use_case.save_view_time_tables;
 
 import ai.classlynk.entity.Timetable;
-import ai.classlynk.entity.User;
 
 import java.util.ArrayList;
 
 public interface SaveViewTimetablesDataAccessInterface {
     //save timetable linked to the user
-    void save(User user, Timetable timetable);
+    void save(Timetable timetable);
 
     //load timetables linked to the user
-    ArrayList<Timetable> load(User user);
+    ArrayList<Timetable> load();
 
-    void delete(User user, Timetable timetable);
+    //delete a timetable linked to the user
+    void delete(Timetable timetable);
 }
