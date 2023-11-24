e1e0aab83c0d6d812fd8bc9a20395ad5ac96f216
Isolumi <hubertx98@gmail.com>
2023-11-24 17:10:23 -0500

    changed main application class to initializer class in order to initialize springboot, which then calls the application class to run the program

--- src/main/java/ai/classlynk/app/ClassLynkApplication.java
+++ src/main/java/ai/classlynk/app/ClassLynkApplication.java
@@ -1,27 +1,17 @@
 package ai.classlynk.app;
 
-import ai.classlynk.data_access.FirebaseRepository;
 import ai.classlynk.interface_adapter.ViewManagerModel;
 import ai.classlynk.interface_adapter.save_view_timetables.SaveViewTimetableController;
 import ai.classlynk.interface_adapter.save_view_timetables.SaveViewTimetableViewModel;
 import ai.classlynk.view.SaveViewTimetableView;
 import ai.classlynk.view.ViewManager;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
-import javax.annotation.Resource;
 import javax.swing.*;
 import java.awt.*;
 
-@SpringBootApplication(scanBasePackages = "ai.classlynk")
 public class ClassLynkApplication {
-    @Resource
-    private FirebaseRepository firebaseRepository;
 
     public static void main(String[] args) {
-        // SpringApplication.run(ClassLynkApplication.class, args);
         JFrame application = new JFrame("ClassLynk");
         application.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
@@ -52,12 +42,4 @@ public class ClassLynkApplication {
         application.pack();
         application.setVisible(true);
     }
-
-    @Bean
-    public CommandLineRunner commandLineRunner() {
-        return args -> {
-            firebaseRepository.insertCourses();
-        };
-    }
-
 }
