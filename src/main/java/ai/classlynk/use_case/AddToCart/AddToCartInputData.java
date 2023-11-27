87bc2db154e38e794c749ec85a6c3c0278607bb6
lichenr3 <chenrui.li@mail.utoronto.ca>
2023-11-27 04:03:28 -0500

    Input data uptodata

--- src/main/java/ai/classlynk/use_case/AddToCart/AddToCartInputData.java
+++ src/main/java/ai/classlynk/use_case/AddToCart/AddToCartInputData.java
@@ -1,23 +1,28 @@
 package ai.classlynk.use_case.AddToCart;
 
+import ai.classlynk.entity.ClassBundle;
 import ai.classlynk.entity.Course;
 import ai.classlynk.entity.SClass;
 
 public class AddToCartInputData {
-    private final Course course;
-    private final SClass sClass;
+    private Course course;
+    private SClass sClass;
+    private ClassBundle classBundle;
 
-    public AddToCartInputData(Course course, SClass sClass) {
+    public AddToCartInputData(Course course, ClassBundle classBundle, SClass sClass) {
         this.course = course;
+        this.classBundle = classBundle;
         this.sClass = sClass;
     }
 
-    public AddToCartInputData(Course course) {
+    public AddToCartInputData(Course course, ClassBundle classBundle) {
         this.course = course;
+        this.classBundle = classBundle;
         this.sClass = null;
     }
 
     public Course getCourse() {return course; }
     public SClass getSClass() {return sClass; }
+    public ClassBundle getClassBundle() {return classBundle; }
 
 }
