d41c01bcf0f59527349947f49b00208cb1e7bece
lichenr3 <chenrui.li@mail.utoronto.ca>
2023-11-27 04:02:17 -0500

    add to cart controller

---
+++ src/main/java/ai/classlynk/interface_adapter/addToCart/AddToCartController.java
@@ -0,0 +1,20 @@
+package ai.classlynk.interface_adapter.addToCart;
+
+import ai.classlynk.entity.ClassBundle;
+import ai.classlynk.entity.Course;
+import ai.classlynk.entity.SClass;
+import ai.classlynk.use_case.AddToCart.AddToCartInputBoundary;
+import ai.classlynk.use_case.AddToCart.AddToCartInputData;
+
+public class AddToCartController {
+    private final AddToCartInputBoundary interactor;
+
+    public AddToCartController(AddToCartInputBoundary interactor) {
+        this.interactor = interactor;
+    }
+
+    public void addToCart(Course course, ClassBundle classBundle, SClass sClass) {
+        AddToCartInputData inputData = new AddToCartInputData(course, classBundle, sClass);
+        interactor.addToCart(inputData);
+    }
+}
