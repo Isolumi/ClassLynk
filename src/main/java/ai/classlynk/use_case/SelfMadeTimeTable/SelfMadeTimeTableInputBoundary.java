a88534bda25c90058748c117d06d554c1198f207
lichenr3 <chenrui.li@mail.utoronto.ca>
2023-11-27 01:02:34 -0500

    delete old version

--- src/main/java/ai/classlynk/use_case/SelfMadeTimeTable/SelfMadeTimeTableInputBoundary.java
+++
@@ -1,5 +0,0 @@
-package ai.classlynk.use_case.SelfMadeTimeTable;
-
-public interface SelfMadeTimeTableInputBoundary {
-    void AddToTimeTable(SelfMadeTimeTableInputData selfMadeTimeTableInputData);
-}
--- src/main/java/ai/classlynk/use_case/SelfMadeTimeTable/SelfMadeTimeTableOutputBoundary.java
+++
@@ -1,5 +0,0 @@
-package ai.classlynk.use_case.SelfMadeTimeTable;
-
-public interface SelfMadeTimeTableOutputBoundary {
-    void presentResponse(SelfMadeTimeTableOutputData outputData);
-}
