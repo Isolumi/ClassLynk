b2e39ae5917f49da678753568032d1d7dff0171f
Isolumi <hubertx98@gmail.com>
2023-12-01 21:28:29 -0500

    temp save

---
+++ src/test/java/ai/classlynk/data_access/FirebaseDataAccessObjectTest.java
@@ -0,0 +1,10 @@
+package ai.classlynk.data_access;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class FirebaseDataAccessObjectTest {
+    @Test
+    void 
+}
