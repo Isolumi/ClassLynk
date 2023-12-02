5923bcdb12ab8be9125e0df4d88953c16d4edd27
Isolumi <hubertx98@gmail.com>
2023-12-01 21:32:28 -0500

    handled merge conf

--- src/main/java/ai/classlynk/use_case/user_auth/register/RegisterInteractor.java
+++ src/main/java/ai/classlynk/use_case/user_auth/register/RegisterInteractor.java
@@ -11,21 +11,13 @@ public class RegisterInteractor implements RegisterInputBoundary{
         RegisterOutputBoundary = registerOutputBoundary;
     }
 
-<<<<<<< HEAD
-    public void execute(RegisterInputData inputData) throws FirebaseAuthException {
-     if (RegisterDAO.existsByUsername(inputData.getName())){
-         RegisterOutputBoundary.fail("Email has been taken");
-        } else if (inputData.getPw1().equals(inputData.getPw2())) {
-         RegisterOutputBoundary.fail("Passwords do not match");
-     }else
-=======
+
     public void execute(RegisterInputData inputData){
-     if (RegisterDAO.existedByName(inputData.getName())){
+     if (RegisterDAO.existsByUsername(inputData.getName())){
          RegisterOutputBoundary.prepareFailView("This name has been used!");
         } else if (!inputData.getPw1().equals(inputData.getPw2())) {
          RegisterOutputBoundary.prepareFailView("Two Passwords are different!");
      }else{
->>>>>>> main
      RegisterDAO.userCreate(inputData.getName(), inputData.getPw2());
      RegisterOutputBoundary.success();
     }
